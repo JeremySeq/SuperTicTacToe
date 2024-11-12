f40e554e53eadaae0a1a20881b3c1a47a2247178
Krazy0range <77130315+Krazy0range@users.noreply.github.com>
2024-11-11 19:06:42 -0600

    Improve code quality, performance, and overall UX

---
+++ src/analysis.py
@@ -0,0 +1,54 @@
+class Analysis:
+
+    def __init__(self, board: list[int] = [0] * 9):
+        self.board: list[int] = board
+
+    def winner(self):
+        vertical_left = self.board[0] == self.board[3] == self.board[6] and self.board[0] != 0
+        vertical_middle = self.board[1] == self.board[4] == self.board[7] and self.board[1] != 0
+        vertical_right = self.board[2] == self.board[5] == self.board[8] and self.board[2] != 0
+        horizontal_top = self.board[0] == self.board[1] == self.board[2] and self.board[0] != 0
+        horizontal_middle = self.board[3] == self.board[4] == self.board[5] and self.board[3] != 0
+        horizontal_bottom = self.board[6] == self.board[7] == self.board[8] and self.board[6] != 0
+        diagonal_topleft_bottomright = self.board[0] == self.board[4] == self.board[8] and self.board[0] != 0
+        diagonal_topright_bottomleft = self.board[2] == self.board[4] == self.board[6] and self.board[2] != 0
+        if (
+            vertical_left
+            or vertical_middle
+            or vertical_right
+            or horizontal_top
+            or horizontal_middle
+            or horizontal_bottom
+            or diagonal_topleft_bottomright
+            or diagonal_topright_bottomleft
+        ):
+            if vertical_middle or horizontal_middle or diagonal_topleft_bottomright or diagonal_topright_bottomleft:
+                return self.board[4]
+            elif vertical_left or horizontal_top:
+                return self.board[0]
+            elif vertical_right or horizontal_bottom:
+                return self.board[8]
+        return 0
+
+    def almost_winners(self) -> dict[int, list[int]]:
+        if self.winner():
+            return None
+
+        board_copy = list(self.board)
+        players = list(set(self.board) - {0})
+        almost_players = dict()
+
+        for i, p in enumerate(self.board):
+            if p != 0:
+                continue
+            for _p in players:
+                self.board[i] = _p
+                if self.winner():
+                    if _p not in almost_players:
+                        almost_players[_p] = [i]
+                    else:
+                        almost_players[_p].append(i)
+                self.board = list(board_copy)
+
+        self.board = list(board_copy)
+        return almost_players
---
+++ src/board.py
@@ -0,0 +1,103 @@
+import pygame
+
+import settings
+from analysis import Analysis
+
+
+class Board:
+
+    def __init__(self, rect: pygame.Rect):
+        self.rect: pygame.Rect = rect
+        self.analysis: Analysis = Analysis()
+
+        self.winner_changed = True
+        self.almost_winners_changed = True
+        self.cached_analysis_winner = None
+        self.cached_analysis_almost_winners = None
+
+    def _rect_grid(self, big_rect: pygame.Rect, i: int) -> pygame.Rect:
+        return pygame.Rect(
+            big_rect.left + (i % 3) * (big_rect.width - settings.BOARD_PADDING) / 3 + settings.BOARD_PADDING,
+            big_rect.top + (i // 3) * (big_rect.height - settings.BOARD_PADDING) / 3 + settings.BOARD_PADDING,
+            (big_rect.width - settings.BOARD_PADDING * 4) / 3,
+            (big_rect.height - settings.BOARD_PADDING * 4) / 3,
+        )
+
+    def draw(self, screen: pygame.Surface, color: pygame.Color):
+        pygame.draw.rect(screen, color, self.rect)
+
+    def winner(self) -> int:
+        if self.winner_changed:
+            self.winner_changed = False
+            self.cached_analysis_winner = self.analysis.winner()
+        return self.cached_analysis_winner
+
+    def almost_winners(self):
+        if self.almost_winners_changed:
+            self.almost_winners_changed = False
+            self.cached_analysis_almost_winners = self.analysis.almost_winners()
+        return self.cached_analysis_almost_winners
+
+    def almost_winning_squares(self):
+        return [v for values in self.almost_winners().values() for v in values]
+
+
+class MetaBoard(Board):
+
+    def __init__(self, rect: pygame.Rect):
+        super().__init__(rect)
+        self.boards: list[TileBoard] = []
+
+        for i in range(9):
+            rect = self._rect_grid(self.rect, i)
+            self.boards.append(TileBoard(rect, self))
+
+    def to_list(self) -> list[int]:
+        return [board.winner() for board in self.boards]
+
+    def update_analysis(self):
+        self.analysis.board = self.to_list()
+        self.cached_analysis_winner = self.analysis.winner()
+        self.cached_analysis_almost_winners = self.analysis.almost_winners()
+
+
+class TileBoard(Board):
+
+    def __init__(self, rect: pygame.Rect, parent: MetaBoard):
+        super().__init__(rect)
+        self.tiles: list[Tile] = []
+        self.parent: MetaBoard = parent
+
+        for i in range(9):
+            rect = self._rect_grid(self.rect, i)
+            self.tiles.append(Tile(rect, self))
+
+    def to_list(self) -> list[int]:
+        return [tile.player for tile in self.tiles]
+
+    def update_analysis(self):
+        self.analysis.board = self.to_list()
+        self.cached_analysis_winner = self.analysis.winner()
+        self.cached_analysis_almost_winners = self.analysis.almost_winners()
+        self.parent.update_analysis()
+
+
+class Tile:
+
+    def __init__(self, rect: pygame.Rect, parent: TileBoard):
+        self.rect: pygame.Rect = rect
+        self.font: pygame.font.SysFont = pygame.freetype.SysFont("3270 Nerd Font Mono", 22)
+        self.player: int = 0
+        self.parent: TileBoard = parent
+
+    def draw(self, screen: pygame.Surface, color: pygame.Color):
+        pygame.draw.rect(screen, color, self.rect)
+
+        letter = settings.PLAYER_SYMBOLS[self.player]
+
+        surf = self.font.render(letter, settings.COLOR_MARK)[0]
+        screen.blit(surf, (self.rect.centerx - 5, self.rect.centery - 4))
+
+    def set_player(self, v):
+        self.player = v
+        self.parent.update_analysis()
